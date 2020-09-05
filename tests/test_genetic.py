bef20534af19e96cb8f785f28dd799b825efff24
MakowToms <makowskit2@student.mini.pw.edu.pl>
2020-09-05 11:23:21 +0200

    Refactor [test] new file with variable set up

--- tests/test_genetic.py
+++ tests/test_genetic.py
@@ -1,29 +1,5 @@
 from genetic_tree import GeneticTree
-from tree.tree import Tree
-from tree.forest import Forest
-from tree.builder import FullTreeBuilder
-
-from sklearn import datasets
-import numpy as np
-
-from sklearn.utils._testing import assert_allclose
-from sklearn.utils._testing import assert_array_equal
-from sklearn.utils._testing import assert_array_almost_equal
-from sklearn.utils._testing import assert_almost_equal
-from sklearn.utils._testing import assert_warns
-from sklearn.utils._testing import assert_warns_message
-from sklearn.utils._testing import create_memmap_backed_data
-from sklearn.utils._testing import ignore_warnings
-
-# load iris dataset and randomly permute it (example from sklearn.tree.test.test_tree)
-iris = datasets.load_iris()
-rng = np.random.RandomState(1)
-perm = rng.permutation(iris.target.size)
-iris.data = iris.data[perm]
-iris.target = iris.target[perm]
-
-X = iris.data
-y = iris.target
+from tests.set_up_variables_and_imports import *
 
 
 def initialize_tree() -> Tree:
@@ -50,7 +26,7 @@ def initialization():
 
 
 def mutate_feature():
-    gt = GeneticTree(change_feature=1, max_depth=1)
+    gt = GeneticTree(change_feature=1, max_depth=1, max_iterations=1)
     gt.fit(X, y)
     forest_before = gt.genetic_processor.forest
     print(f'Features of tree before mutation: {forest_before.trees[0].feature}')
@@ -62,5 +38,5 @@ def mutate_feature():
 
 if __name__ == "__main__":
     forest: Forest = mutate_feature()
-    for tree in forest.trees[1000:1050]:
+    for tree in forest.trees[1000:1040]:
         print(tree.feature)
