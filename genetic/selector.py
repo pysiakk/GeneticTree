fdca6a3b206b84b3e08f9443da16322facbf6407
MakowToms <makowskit2@student.mini.pw.edu.pl>
2020-11-10 15:28:43 +0100

    Feature [selector] tournament selection + tests for all selctor

--- genetic/selector.py
+++ genetic/selector.py
@@ -10,6 +10,19 @@ def get_selected_indices_by_rank_selection(metrics, n_individuals):
     return indices
 
 
+def get_selected_indices_by_tournament_selection(metrics, n_individuals, tournament_size=3):
+    random_indices = np.empty([n_individuals, tournament_size], dtype=np.int)
+    for i in range(tournament_size):
+        random_indices[:, i] = np.random.randint(0, metrics.shape[0] - i, n_individuals)
+
+    def tournament_selection(row):
+        for j in range(1, row.shape[0]):
+            row[j] += np.sum(row[:j] <= row[j])
+        return row[np.argmax(metrics[row])]
+
+    return np.apply_along_axis(tournament_selection, 1, random_indices)
+
+
 class SelectionType(Enum):
     """
     SelectionType is enumerator with possible selections to use:
@@ -40,6 +53,7 @@ class SelectionType(Enum):
     # (also can be more arguments which will be ignored)
     # this is needed because value is callable type
     RankSelection = get_selected_indices_by_rank_selection,
+    TournamentSelection = get_selected_indices_by_tournament_selection,
 
 
 class Selector:
@@ -64,14 +78,13 @@ class Selector:
                  selection_type: SelectionType = SelectionType.RankSelection,
                  n_elitism: int = 5,
                  **kwargs):
-        assert n_elitism <= n_trees
-        self.n_trees: int = n_trees
-        self.selection_type: SelectionType = selection_type
-        self.n_elitism: int = n_elitism
+        self.n_trees: int = self._check_n_trees_(n_trees)
+        self.selection_type: SelectionType = self._check_selection_type_(selection_type)
+        self.n_elitism: int = self._check_n_elitism_(n_elitism)
 
     def set_params(self,
                    n_trees: int = None,
-                   selection_type: callable = None,
+                   selection_type: SelectionType = None,
                    n_elitism: int = None,
                    **kwargs):
         """
@@ -80,12 +93,34 @@ class Selector:
         Arguments are the same as in __init__
         """
         if n_trees is not None:
-            self.n_trees = n_trees
+            self.n_trees = self._check_n_trees_(n_trees)
         if selection_type is not None:
-            self.selection_type = selection_type
+            self.selection_type = self._check_selection_type_(selection_type)
         if n_elitism is not None:
-            assert n_elitism <= self.n_trees
-            self.n_elitism = n_elitism
+            self.n_elitism = self._check_n_elitism_(n_elitism)
+
+    @staticmethod
+    def _check_n_trees_(n_trees):
+        if n_trees <= 0:
+            warnings.warn(f"Try to set n_trees={n_trees}. Changed to n_trees=1, "
+                          f"but try to set n_trees manually for value at least 20")
+            n_trees = 1
+        return n_trees
+
+    @staticmethod
+    def _check_selection_type_(selection_type):
+        if isinstance(selection_type, SelectionType):
+            return selection_type
+        else:
+            raise TypeError(f"Passed selection_type={selection_type} with type {type(selection_type)}, "
+                            f"Needed argument with type SelectionType")
+
+    def _check_n_elitism_(self, n_elitism):
+        if n_elitism >= self.n_trees:
+            n_elitism = self.n_trees
+        if n_elitism <= 0:
+            n_elitism = 0
+        return n_elitism
 
     def select(self, trees, trees_metrics):
         """
@@ -105,6 +140,7 @@ class Selector:
                           f"probability with do not replacing parents.",
                           UserWarning)
         indices = self.selection_type.select(trees_metrics, self.n_trees)
+        # TODO take only once each index, then if index is repeated make a copy of tree
         new_trees = list(np.take(np.array(trees), indices))
         return new_trees
 
@@ -119,7 +155,7 @@ class Selector:
         n_elitism = self.n_elitism
         if n_elitism <= 0:
             return []
-        elif n_elitism <= len(trees):
+        elif n_elitism >= len(trees):
             n_elitism = len(trees)
         elite_indices = np.argpartition(-trees_metrics, n_elitism - 1)[:n_elitism]
         return list(np.take(np.array(trees), elite_indices))
---
+++ tests/test_selector.py
@@ -0,0 +1,167 @@
+from genetic.selector import SelectionType, Selector
+from genetic.selector import get_selected_indices_by_rank_selection
+from genetic.selector import get_selected_indices_by_tournament_selection
+
+from sklearn.utils._testing import assert_array_equal
+
+import numpy as np
+import pytest
+
+
+# ==============================================================================
+# Selection functions
+# ==============================================================================
+@pytest.fixture
+def metrics():
+    return np.array([18,  5, 12, 11,  20, 17,  8, 10,  6,  5,  3,  2, 14,  1,  4])
+
+
+# +++++++++++++++
+# Rank selection
+# +++++++++++++++
+@pytest.mark.parametrize("n_individuals",
+                         [i for i in range(3, 18, 3)])
+def test_rank_selection(metrics, n_individuals):
+    selected_indices = get_selected_indices_by_rank_selection(metrics, n_individuals)
+    proper_indices = np.argsort(-metrics)[:n_individuals]
+    assert_array_equal(np.sort(selected_indices), np.sort(proper_indices))
+
+
+# +++++++++++++++
+# Tournament selection
+# +++++++++++++++
+def tournament_selection(metrics, n_individuals, tournament_size, proper_array):
+    np.random.seed(123)
+    selected_indices = get_selected_indices_by_tournament_selection(metrics, n_individuals, tournament_size)
+    assert_array_equal(selected_indices, proper_array)
+
+
+@pytest.mark.parametrize("n_individuals, tournament_size",
+                         [(10, 3), (10, 5)])
+def test_tournament_selection_reproducible_results(metrics, n_individuals, tournament_size):
+    np.random.seed(123)
+    result1 = get_selected_indices_by_tournament_selection(metrics, n_individuals, tournament_size)
+    np.random.seed(123)
+    result2 = get_selected_indices_by_tournament_selection(metrics, n_individuals, tournament_size)
+    assert_array_equal(result1, result2)
+
+
+def test_tournament_selection1(metrics):
+    tournament_selection(metrics, 10, 3, np.array([3, 4, 0, 0, 0, 2, 6, 0, 0, 4]))
+
+
+def test_tournament_selection2(metrics):
+    tournament_selection(metrics, 10, 5, np.array([3, 4, 0, 0, 0, 2, 3, 0, 0, 4]))
+
+
+def test_tournament_selection_with_size_1(metrics):
+    np.random.seed(123)
+    proper_array = np.random.randint(0, 15, 10)
+    tournament_selection(metrics, 10, 1, proper_array)
+
+
+# +++++++++++++++
+# Roulette selection
+# +++++++++++++++
+
+
+# +++++++++++++++
+# Stochastic uniform selection
+# +++++++++++++++
+
+
+# ==============================================================================
+# Selector and SelectionType
+# ==============================================================================
+
+# Base selector
+@pytest.fixture
+def selector():
+    return Selector(10, SelectionType.RankSelection, 3)
+
+
+@pytest.fixture
+def trees():
+    return np.random.random(15)
+
+
+# +++++++++++++++
+# Set params
+# +++++++++++++++
+
+@pytest.mark.parametrize("n_trees", [1, 5, 1000])
+def test_set_n_trees(selector, n_trees):
+    selector.set_params(n_trees=n_trees)
+    assert selector.n_trees == n_trees
+
+
+@pytest.mark.parametrize("n_trees", [-10, 0])
+def test_set_n_trees_below_one(selector, n_trees):
+    selector.set_params(n_trees=n_trees)
+    assert selector.n_trees == 1
+
+
+@pytest.mark.parametrize("selection_type", [SelectionType.RankSelection,
+                                            SelectionType.TournamentSelection])
+def test_set_selection_type(selector, selection_type):
+    selector.set_params(selection_type=selection_type)
+    assert selector.selection_type == selection_type
+
+
+@pytest.mark.parametrize("selection_type", ["some_string"])
+def test_set_selection_type_with_wrong_type(selector, selection_type):
+    with pytest.raises(TypeError):
+        selector.set_params(selection_type=selection_type)
+
+
+@pytest.mark.parametrize("n_elitism", [1, 5])
+def test_set_n_elitism(selector, n_elitism):
+    selector.set_params(n_elitism=n_elitism)
+    assert selector.n_elitism == n_elitism
+
+
+def test_set_n_elitism_below_zero(selector):
+    selector.set_params(n_elitism=-10)
+    assert selector.n_elitism == 0
+
+
+def test_set_n_elitism_above_n_trees(selector):
+    selector.set_params(n_elitism=15)
+    assert selector.n_elitism == 10
+
+
+# +++++++++++++++
+# Elitism
+# +++++++++++++++
+
+def test_elitism(selector, metrics, trees):
+    elite_population = selector.get_elite_population(trees, metrics)
+    elite_indices = np.argsort(-metrics)[:selector.n_elitism]
+    assert_array_equal(np.sort(trees[elite_indices]), np.sort(elite_population))
+
+
+def test_elitism_below_zero(selector, metrics, trees):
+    selector.set_params(n_elitism=-10)
+    assert_array_equal(selector.get_elite_population(trees, metrics), [])
+
+
+@pytest.mark.parametrize("trees_len", [3, 7, 9])
+def test_elitism_above_trees_len(selector, metrics, trees, trees_len):
+    selector.set_params(n_elitism=10)
+    assert selector.get_elite_population(trees[:trees_len], metrics[:trees_len]).__len__() == trees_len
+
+
+# +++++++++++++++
+# SelectionTypes
+# +++++++++++++++
+
+@pytest.mark.parametrize("selection_type", [SelectionType.RankSelection,
+                                            SelectionType.TournamentSelection])
+def test_calling_proper_selection_type(selector, metrics, trees, selection_type):
+    selector.set_params(selection_type=selection_type)
+    np.random.seed(123)
+    trees_by_selector = selector.select(trees, metrics)
+    np.random.seed(123)
+    indices_by_selection_type = selection_type.select(metrics, selector.n_trees)
+    assert_array_equal(trees[indices_by_selection_type], trees_by_selector)
+
