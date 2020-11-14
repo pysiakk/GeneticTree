61fa89dda4559c91c1914458d80bb74f04f4acf1
MakowToms <makowskit2@student.mini.pw.edu.pl>
2020-11-14 22:06:35 +0100

    Refactor [crosser] first choose random parents, then cross them

--- genetic/crosser.py
+++ genetic/crosser.py
@@ -49,44 +49,59 @@ class Crosser:
 
         trees_number: int = len(trees)
 
-        for first_parent_id in range(trees_number):
-            first_parent: Tree = trees[first_parent_id]
-            if np.random.rand() < self.cross_prob:
-                # find second parent
-                second_parent_id: int = self.get_second_parent(trees_number, first_parent_id)
-                second_parent: Tree = trees[second_parent_id]
+        first_parents_indices: np.array = self._get_random_trees_(trees_number, self.cross_prob)
+        second_parents_indices: np.array = self._get_second_parents_(trees_number, first_parents_indices)
 
-                # create child and register it in forest
-                children: Tree[:] = cross_trees(first_parent, second_parent, int(self.cross_is_both))
+        for i in range(first_parents_indices.shape[0]):
+            first_parent: Tree = trees[first_parents_indices[i]]
+            second_parent: Tree = trees[second_parents_indices[i]]
 
-                # TODO change below lines to more complicated way that should be less time consuming
-                # During copying nodes from first tree copy also all observations dict
-                # and replace observations below changed node as NOT_REGISTERED
-                # Then after completion of all tree only need to run assign_all_not_registered_observations
-                children[0].initialize_observations()
-                if self.cross_is_both:
-                    children[1].initialize_observations()
+            # create child and register it in forest
+            children: Tree[:] = cross_trees(first_parent, second_parent, int(self.cross_is_both))
 
-                new_created_trees.append(children[0])
-                if self.cross_is_both:
-                    new_created_trees.append(children[1])
+            # TODO change below lines to more complicated way that should be less time consuming
+            # During copying nodes from first tree copy also all observations dict
+            # and replace observations below changed node as NOT_REGISTERED
+            # Then after completion of all tree only need to run assign_all_not_registered_observations
+            children[0].initialize_observations()
+            if self.cross_is_both:
+                children[1].initialize_observations()
+
+            new_created_trees.append(children[0])
+            if self.cross_is_both:
+                new_created_trees.append(children[1])
 
         return new_created_trees
 
     @staticmethod
-    def get_second_parent(n_trees: int, first_parent_id: int) -> int:
+    def _get_random_trees_(n_trees: int, probability: float) -> np.array:
+        """
+        Args:
+            n_trees: Number of trees
+            probability: Probability of choosing each individual
+
+        Returns:
+            np.array with indices
+        """
+        indices: np.array = np.arange(0, n_trees)
+        random_values: np.array = np.random.random(n_trees)
+        return indices[random_values < probability]
+
+    @staticmethod
+    def _get_second_parents_(n_trees: int, first_parents: np.array) -> np.array:
         """
         Function to choose another individual (to cross with) from uniform
         distribution of other individuals
+        It choose second individual for each individual in first_parents array
 
         Args:
-            n_trees: Number of trees in the forest
-            first_parent_id: Id of first chosen parent
+            first_parents: Array with indices of first parents
+            n_trees: Number of trees
 
         Returns:
-            Id of second parent such that it is a number from 0 to n_trees - 1 other than first parent id
+            Array of ids of second parents such that each id is a number from 0 \
+            to n_trees - 1 other than first parent id
         """
-        second_parent: int = np.random.randint(0, n_trees-1)
-        if second_parent >= first_parent_id:
-            second_parent += 1
-        return second_parent
+        second_parents: np.array = np.random.randint(0, n_trees-1, first_parents.shape[0])
+        second_parents += (second_parents >= first_parents)
+        return second_parents
