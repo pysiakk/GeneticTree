0c2af6fdc8fd88cf05002fdedf1932666346e62d
pysiakk <karol.pysiak.13@gmail.com>
2020-12-08 20:00:17 +0100

    Cleanup test_stop

--- tests/test_stop.py
+++ tests/test_stop.py
@@ -1,12 +1,10 @@
 import os
 os.chdir("../")
 
-from genetic.initializer import Initializer, InitializationType
 from tests.set_up_variables_and_imports import *
 from tree.thresholds import prepare_thresholds_array
 from genetic_tree import GeneticTree
 from genetic.stop_condition import StopCondition
-from tree.tree import Tree
 
 X = GeneticTree._check_X_(GeneticTree(), X, True)
 thresholds = prepare_thresholds_array(10, X)
