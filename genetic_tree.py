2abedcd35533ea9497bc4d2b08859349e0ba92b5
MakowToms <makowskit2@student.mini.pw.edu.pl>
2021-01-05 15:52:53 +0100

    TODO [genetic_tree] initialization of trees in new training

--- genetic_tree.py
+++ genetic_tree.py
@@ -137,6 +137,7 @@ class GeneticTree:
         thresholds = prepare_thresholds_array(self._n_thresholds, X)
         if self._trees is None:  # when previously trees was removed
             self._trees = self.initializer.initialize(X, y, sample_weight, thresholds)
+        # TODO if previous trees not removed: add new X, y, sample_weights and X
 
     def _growth_trees(self):
         offspring = self._trees
