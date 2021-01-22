99fc3d080b2c211d1873d080ec82486acb89b90a
pysiakk <karol.pysiak.13@gmail.com>
2021-01-22 13:08:26 +0100

    DOC [stopper] Added docstrings to stopper

--- genetic_tree/genetic/stopper.py
+++ genetic_tree/genetic/stopper.py
@@ -6,10 +6,8 @@ class Stopper:
     Class responsible for checking stop condition in genetic algorithm
 
     Implemented conditions:
-    maximum number of iterations
-
-    In development:
-    maximum number of iterations without improvement of best individual
+    -- maximum number of iterations
+    -- maximum number of iterations without improvement of best individual
     """
 
     def __init__(self, max_iter: int = 500,
@@ -25,6 +23,11 @@ class Stopper:
     def set_params(self, max_iter: int = None,
                    n_iter_no_change: int = None, early_stopping: bool = None,
                    **kwargs):
+        """
+        Function to set new parameters for Stopper
+
+        Arguments are the same as in __init__
+        """
         if max_iter is not None:
             self.max_iter = max_iter
         if n_iter_no_change is not None:
@@ -33,12 +36,23 @@ class Stopper:
             self.early_stopping = early_stopping
 
     def reset_private_variables(self):
+        """
+        Function that resets all private variables of the stopper class to their default values.
+        """
         self.current_iteration: int = 1
         self.best_result: float = -math.inf
         self.best_result_iteration: int = 1
         self.best_metric_hist: list = []
 
     def stop(self, metrics: list = None) -> bool:
+        """
+
+        Args:
+            metrics: An array of values of the fitness function based on trees from a current generation
+
+        Returns:
+            True if the learning should be stopped. False if the learning should go on.
+        """
         score = max(metrics)
         if self.current_iteration > self.max_iter:
             return True
